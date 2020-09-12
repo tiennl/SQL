c69b488cfc02eabd943fcdb4ff9bb14541ef892f
tiennl <ngolethuytien@gmail.com>
2020-09-12 11:53:35 +0700

    chuong 5

--- NHANVIEN.sql
+++ NHANVIEN.sql
@@ -93,6 +93,9 @@ UPDATE NHANVIEN SET MaPHG = 'P002' WHERE MaNV = 'NV002'
 UPDATE NHANVIEN SET MaPHG = 'P003' WHERE MaNV = 'NV003'
 
 INSERT INTO NHANVIEN(MaNV, TenNV, HoNV, NgaySinh, DiaChi, Phai, Luong, MaNQL, MaPHG) VALUES
+('NV013', 'Thanh', 'Le', '1899-07-25', 'Son Tra', 'Nam', 4500000, 'NV003', 'P004'),
+('NV011', 'An', 'Tran', '1896-07-25', 'Son Tra', 'Nam', 5500000, 'NV001', 'P005'),
+('NV012', 'Hanh', 'Vo', '1890-07-25', 'Lien Chieu', 'Nu', 5800000, 'NV002', 'P004'),
 ('NV004', 'Khanh', 'Le', '1880-07-25', 'Lien Chieu', 'Nam', 5000000, 'NV001', 'P001'),
 ('NV005', 'Anh', 'Nguyen', '1990-10-10', 'Cam Le', 'Nu', 4500000, 'NV001', 'P001'),
 ('NV006', 'Uyen', 'Pham', '1991-09-20', 'Lien Chieu', 'Nu', 700000, 'NV002', 'P002'),
@@ -109,13 +112,16 @@ INSERT INTO DIADIEMPHG VALUES
 ('P003', 'Son Tra')
 
 INSERT INTO THANNHAN(MaNV, TenTN, Phai, NgaySinh, QuanHe) VALUES
-('NV001', 'Nguyen Hai Son', 'Nam', '2000-10-21', 'Con'),
-('NV001', 'Nguyen Minh Anh', 'Nu', '2002-09-04', 'Con'),
-('NV003', 'Tran Minh', 'Nam', '1973-10-21', 'Chong'),
-('NV004', 'Le Thanh', 'Nam', '2010-04-21', 'Con'),
-('NV005', 'Le Chi', 'Nu', '1960-10-25', 'Me')
+('NV001', 'Son', 'Nam', '2000-10-21', 'Con'),
+('NV001', 'Anh', 'Nu', '2002-09-04', 'Con'),
+('NV003', 'Minh', 'Nam', '1973-10-21', 'Chong'),
+('NV004', 'Thanh', 'Nam', '2010-04-21', 'Con'),
+('NV005', 'Chi', 'Nu', '1960-10-25', 'Me'),
+('NV007', 'Ha', 'Nam', '1996-12-20', 'Anh')
 
 INSERT INTO DEAN(MaDA, TenDA, DiaDiemDA, MaPHG) VALUES 
+('DA007', 'San pham YY', 'Ha Noi', 'P002'),
+('DA008', 'San pham X', 'Ha Noi', 'P004'),
 ('DA001', 'He thong quan ly khach san', 'Son Tra', 'P001'),
 ('DA002', 'He thong quan ly sinh vien', 'Hai Chau', 'P002'),
 ('DA003', 'He thong quan ly nha hang', 'Lien Chieu', 'P003'),
@@ -124,6 +130,10 @@ INSERT INTO DEAN(MaDA, TenDA, DiaDiemDA, MaPHG) VALUES
 ('DA006', 'Phan mem trac nghiem Tieng Anh', 'Cam Le','P002')
 
 INSERT INTO PHANCONG(MaNV, MaDA, ThoiGian) VALUES
+('NV001','DA007', 180),
+('NV001','DA008', 200),
+('NV011','DA008', 180),
+('NV012','DA008', 200),
 ('NV001','DA003', 150),
 ('NV001','DA004', 150),
 ('NV004','DA004', 100),
@@ -200,3 +210,153 @@ FROM PHANCONG PC JOIN DEAN DA ON PC.MaDA = DA.MaDA
 WHERE DA.MaPHG = 'P004'
 GROUP BY PC.MaNV
 HAVING COUNT(PC.MaNV) = (SELECT COUNT(MaDA) FROM DEAN GROUP BY MaPHG HAVING MaPHG = 'P004')
+
+------------------------------------------------------------------------------------------------------
+-------------------------------------------- CHUONG 5 SQL --------------------------------------------
+------------------------------------------------------------------------------------------------------
+
+-- 1. Với những đề án ở ‘Ha Noi’, cho biết mã đề án, mã phòng ban chủ trì đề án, họ tên trưởng phòng 
+-- cùng với ngày sinh và địa chỉ của người ấy
+SELECT DA.MaDA, DA.MaPHG,  (NV.HoNV + ' ' + NV.TenNV) AS HoTenTrPHG, NV.NgaySinh, NV.DiaChi 
+FROM DEAN DA JOIN PHONGBAN PB ON DA.MaPHG = PB.MaPHG 
+JOIN NHANVIEN NV ON PB.TruongPHG = NV.MaNV 
+WHERE DA.DiaDiemDA = 'Ha Noi'
+
+-- 2. Tìm họ tên của nhân viên phòng số 5 có tham gia vào đề án “Sản phẩm X” với số giờ làm việc trên 10 giờ
+SELECT (NV.HoNV + ' ' + NV.TenNV) AS HoTenNV
+FROM NHANVIEN NV JOIN PHANCONG PC ON NV.MaNV = PC.MaNV
+JOIN DEAN DA ON PC.MaDA = DA.MaDA
+WHERE NV.MaPHG = 'P005' AND DA.TenDA = 'San pham X' AND PC.ThoiGian > 10 
+
+-- 3. Tìm họ tên của từng nhân viên và người phụ trách trực tiếp nhân viên đó
+SELECT (NV.HoNV + ' ' + NV.TenNV) AS HoTenNV, (NQL.HoNV + ' ' + NQL.TenNV) AS HoTenNQL
+FROM NHANVIEN NV LEFT JOIN NHANVIEN NQL ON NV.MaNQL = NQL.MaNV 
+
+-- 4. Tìm họ tên của những nhân viên được “Tung” phụ trách trực tiếp
+SELECT (NV.HoNV + ' ' + NV.TenNV) AS HoTenNV
+FROM NHANVIEN NV JOIN NHANVIEN NQL ON NV.MaNQL = NQL.MaNV 
+WHERE NQL.TenNV = 'Tung'
+
+-- 5. Cho biết các mã đề án có:
+-- Nhân viên với họ là ‘Nguyen’ tham gia hoặc,
+-- Trưởng phòng chủ trì đề án đó với họ là ‘Nguyen’
+(SELECT DA.MaDA 
+FROM DEAN DA JOIN PHONGBAN PB ON DA.MaPHG = PB.MaPHG 
+JOIN NHANVIEN NV ON PB.TruongPHG = NV.MaNV
+WHERE NV.HoNV = 'Nguyen')
+UNION
+(SELECT DA.MaDA
+FROM DEAN DA JOIN PHANCONG PC ON DA.MaDA = PC.MaDA 
+JOIN NHANVIEN NV ON PC.MaNV = NV.MaNV
+WHERE NV.HoNV = 'Nguyen')
+
+-- 6. Tìm nhân viên có người thân cùng tên và cùng giới tính
+SELECT NV.MaNV
+FROM NHANVIEN NV JOIN THANNHAN TN ON NV.MaNV = TN.MaNV
+WHERE NV.TenNV = TN.TenTN AND NV.Phai = TN.Phai
+
+-- 7. Tìm những nhân viên không có thân nhân nào
+SELECT MaNV 
+FROM NHANVIEN 
+WHERE MaNV NOT IN 
+(SELECT NV.MaNV FROM NHANVIEN NV JOIN THANNHAN TN ON NV.MaNV = TN.MaNV)
+
+-- 8. Tìm những nhân viên có lương lớn hơn lương của ít nhất một nhân viên phòng 4
+SELECT MaNV 
+FROM NHANVIEN WHERE Luong > (SELECT MIN(Luong) FROM NHANVIEN WHERE MaPHG = 'P004')
+
+-- 9. Tìm những nhân viên có lương lớn hơn lương của tất cả nhân viên phòng 4
+SELECT MaNV
+FROM NHANVIEN WHERE Luong > (SELECT MAX(Luong) FROM NHANVIEN WHERE MaPHG = 'P004')
+
+-- 10. Tìm những trưởng phòng có tối thiểu một thân nhân
+SELECT DISTINCT PB.TruongPHG
+FROM PHONGBAN PB JOIN NHANVIEN NV ON PB.TruongPHG = NV.MaNV
+JOIN THANNHAN TN ON NV.MaNV = TN.MaNV
+
+-- 11. Tìm tên các nhân viên được phân công làm tất cả các đồ án
+SELECT MaNV
+FROM PHANCONG
+GROUP BY MaNV
+HAVING COUNT(MaDA) = (SELECT COUNT(MaDA) FROM DEAN)
+
+-- 12. Tìm tổng lương, lương cao nhất, lương thấp nhất và lương trung bình của các nhân viên
+SELECT SUM(Luong) AS TongLuong, MAX(Luong) AS LuongCaoNhat, MIN(Luong) AS LuongThapNhat, AVG(Luong) AS LuongTB
+FROM NHANVIEN
+
+-- 13. Cho biết số lượng nhân viên và lương trung bình của phòng ‘Nghien cuu’
+SELECT COUNT(MaNV) AS SoLuongNV, AVG(Luong) AS LuongTB
+FROM NHANVIEN NV JOIN PHONGBAN PB ON NV.MaPHG = PB.MaPHG
+WHERE PB.TenPHG = 'Nghien cuu'
+
+-- 14. Cho biết số lượng nhân viên của từng phòng ban. Xuất ra tên phòng và số lượng.
+SELECT PB.MaPHG, COUNT(NV.MaNV) AS SoLuongNV
+FROM NHANVIEN NV JOIN PHONGBAN PB ON NV.MaPHG = PB.MaPHG
+GROUP BY PB.MaPHG
+ORDER BY PB.MaPHG
+
+-- 15. Với mỗi nhân viên cho biết mã số, họ tên, số lượng đề án và tổng thời gian mà họ tham gia
+SELECT NV.MaNV, COUNT(PC.MaDA) AS SoDA, SUM(PC.ThoiGian) AS TongTG
+FROM NHANVIEN NV JOIN PHANCONG PC ON NV.MaNV = PC.MaNV
+JOIN DEAN DA ON PC.MaDA = DA.MaDA
+GROUP BY NV.MaNV
+
+-- 16. Cho biết những nhân viên tham gia từ 2 đề án trở lên 
+SELECT MaNV
+FROM PHANCONG 
+GROUP BY MaNV
+HAVING COUNT(MaDA) >= 2
+
+-- 17. Cho biết những phòng ban (TENPHG) có lương trung bình của các nhân viên lớn lơn 20000
+SELECT PB.TenPHG, AVG(NV.Luong) AS LuongTB
+FROM NHANVIEN NV JOIN PHONGBAN PB ON NV.MaPHG = PB.MaPHG
+GROUP BY PB.TenPHG
+HAVING AVG(NV.Luong) > 20000
+
+-- 18. Tìm những phòng ban có lương trung bình cao nhất
+SELECT TOP 1 PB.TenPHG, AVG(NV.Luong) AS LuongTB
+FROM NHANVIEN NV JOIN PHONGBAN PB ON NV.MaPHG = PB.MaPHG
+GROUP BY PB.TenPHG
+ORDER BY LuongTB DESC
+
+-- 19. Tìm 3 nhân viên có lương cao nhất
+SELECT TOP 3 MaNV, Luong
+FROM NHANVIEN
+ORDER BY Luong DESC
+
+-- 20. Tìm mã và tên các nhân viên làm việc tại phòng ‘Nghien cuu’
+SELECT NV.MaNV, NV.TenNV
+FROM NHANVIEN NV JOIN PHONGBAN PB ON NV.MaPHG = PB.MaPHG
+WHERE PB.TenPHG = 'Nghien cuu'
+
+-- 21. Tìm họ tên các nhân viên và tên các đề án nhân viên tham gia nếu có
+SELECT (NV.HoNV + ' ' + NV.TenNV) AS HoTenNV, DA.TenDA
+FROM NHANVIEN NV JOIN PHANCONG PC ON NV.MaNV = PC.MaNV
+JOIN DEAN DA ON PC.MaDA = DA.MaDA
+
+-- 22. Cho biết họ tên các nhân viên và tuổi về hưu của họ (nam 60 tuổi, nữ 55 tuổi)
+
+
+-- 23. Cho biết họ tên các nhân viên và năm về hưu
+
+
+-- 24. Xóa đi những nhân viên ở phòng ‘Nghien cuu’
+
+
+-- 25. Với đề án có mã số 10, hãy thay đổi nơi thực hiện đề án thành ‘Vung Tau’ và phòng ban phụ trách là phòng 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
